04a1c7a3ac0df6a4f199b474c185a03dc5cda627
Damien Retzinger <damienwebdev@gmail.com>
2022-06-29 20:11:09 -0400

    ci: add unit, integration, and installation test

---
+++ Test/Unit/Integration/SmokeTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Jomashop
+ */
+
+namespace JomaShop\NewRelicMonitoring\Test\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+class SmokeTest extends TestCase
+{
+    public function testItworks()
+    {
+        $this->assertEquals(true, true);
+    }
+}
